f200058b7577152c2c84a45c72cd19b5fc9b370d
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-12-12 23:27:41 -0200

    Update Welcome.tsx

--- Nlw-05-plantManger/src/pages/Welcome.tsx
+++ Nlw-05-plantManger/src/pages/Welcome.tsx
@@ -9,15 +9,13 @@ import {
     View
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/core';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
-export function Welcome(){
-    const navigation = useNavigation();
-
+export function Welcome({ navigation }){
+    
     function handleStart(){
         navigation.navigate('UserIdentification');        
     }
